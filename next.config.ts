6891292534419f66592a5f011382adc17d450df8
Shekhar Ramola <shekharramola2012@gmail.com>
2026-08-24 20:51:07 +0530

    chore: fix dynamic routing attempt 1/n

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  // output: process.env.NODE_ENV === "production" ? "export" : undefined,
+
   distDir: "out",
+  trailingSlash: true,
   /* CRITICAL FOR SERVERLESS ISOLATE RENDERS */
   images: {
     unoptimized: true,
