17900355a89022c59311faf2434540152f8d4bec
Shekhar Ramola <shekharramola2012@gmail.com>
2026-08-28 19:59:50 +0530

    chore: added new env variable

--- src/@types/api-contract.d.ts
+++ src/@types/api-contract.d.ts
@@ -26,6 +26,39 @@ declare const routes: import("hono/hono-base").HonoBase<
             | {
                 input: {};
                 output: {
+                  error: {
+                    message: string;
+                    statusCode: number | null;
+                    name:
+                      | "invalid_idempotency_key"
+                      | "validation_error"
+                      | "missing_api_key"
+                      | "restricted_api_key"
+                      | "invalid_api_key"
+                      | "not_found"
+                      | "method_not_allowed"
+                      | "invalid_idempotent_request"
+                      | "concurrent_idempotent_requests"
+                      | "invalid_attachment"
+                      | "invalid_from_address"
+                      | "invalid_access"
+                      | "invalid_parameter"
+                      | "invalid_region"
+                      | "missing_required_field"
+                      | "monthly_quota_exceeded"
+                      | "daily_quota_exceeded"
+                      | "rate_limit_exceeded"
+                      | "security_error"
+                      | "application_error"
+                      | "internal_server_error";
+                  };
+                };
+                outputFormat: "json";
+                status: 400;
+              }
+            | {
+                input: {};
+                output: {
                   sent: true;
                 };
                 outputFormat: "json";
